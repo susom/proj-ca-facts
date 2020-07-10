064ae60d20f8a5e68fc08189de0b17b154270cb7
Irvin Szeto <irvins@stanford.edu>
2020-07-09 18:44:35 -0700

    WIP placeholder for kit submission

---
+++ pages/kitsubmission.php
@@ -0,0 +1,19 @@
+<?php
+namespace Stanford\ProjCaFacts;
+/** @var \Stanford\ProjCaFacts\ProjCaFacts $module */
+
+
+$module->emLog($_REQUEST, "Incoming Request - Form Post expecting Access Code + Zip Code" . " " . __DIR__);
+if (!$module->parseFormInput()) {
+    $module->returnError("Invalid Request Parameters - check your syntax");
+}
+
+// Response is handled by $module
+$module->formHandler();
+
+/*
+
+1) Get code + ZIP from post
+2) ajax call to this page to verify code and zip match - if so, then create a new record with the code and redirect to survey
+
+ */
